fa1e69b7fdf8eedafa2f9b6025dc99e3de56758b
JujubinhaBingBing <julia.leithold@outlook.com>
2026-07-15 23:15:19 -0300

    Add 1 new photos to gallery

---
+++ data/gallery.js
@@ -0,0 +1,50 @@
+export const data = {
+  author: {
+  "id": "jujuba",
+  "name": "Jujuba",
+  "icon": "",
+  "githubUser": "jujubinhabingbing",
+  "background": "",
+  "color": "#d02be6",
+  "summary": "Breve biografia sobre o autor...",
+  "socials": [
+    {
+      "name": "Instagram",
+      "url": "https://instagram.com/..."
+    }
+  ],
+  "allowDownloads": true,
+  "bannerStyle": "standard",
+  "galleryStyle": "masonry",
+  "gitGalleryPath": "data/authors/jujubinhabingbing/gallery.js",
+  "gitCollectionsPath": "data/authors/jujubinhabingbing/collection.js",
+  "privateGalleryPath": "data/gallery.js",
+  "privateCollectionsPath": "data/collections.js"
+},
+  images: [
+  {
+    "id": "PM-44T857",
+    "title": "The First Descendant",
+    "date": "17/03/2026",
+    "tbn": "https://i.ibb.co/chmQPCm8/tbn-PM-RGH34-H-1784168026923.jpg",
+    "src": "https://i.ibb.co/hx5trNW0/PM-RGH34-H-1784168026923.jpg",
+    "width": 1920,
+    "height": 1080,
+    "dominantColor": "#302020",
+    "palette": [
+      "#302020",
+      "#908080",
+      "#605050",
+      "#80b0b0",
+      "#b0d0e0"
+    ],
+    "words": [
+      "tfd"
+    ],
+    "characters": [
+      "Valby"
+    ],
+    "extras": []
+  }
+]
+};
